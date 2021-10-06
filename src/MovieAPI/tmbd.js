f67daebf731a3e7adb7809851bfc7ffd63866356
Sarthak_SarthakGarg <sarthakg@leewayhertz.com>
2021-10-06 14:12:20 +0530

    upgraded

--- src/MovieAPI/tmbd.js
+++ src/MovieAPI/tmbd.js
@@ -3,7 +3,7 @@ const API_BASE = "https://api.themoviedb.org/3";
 
 
 const basicFetch = async (endpoint) => {
-    const req =await fetch(`${API_BASE}${endpoint}`);
+    const req =await fetch(`/api/* ${API_BASE}${endpoint}`);
     const json = await req.json();
     return json
 }
